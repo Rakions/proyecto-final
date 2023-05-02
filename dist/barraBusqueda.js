7624b0f417d09dbbee9bac614906313e6dacd82e
Rakions <ogmohammed2004@hotmail.com>
2023-05-02 19:44:36 +0200

    Seccion categorias hecho

--- dist/barraBusqueda.js
+++ dist/barraBusqueda.js
@@ -5,39 +5,41 @@ const tablaOpciones = document.getElementById("search-results");
 
 //cuando escribamos ejecuta el codigo
 barraBusqueda.addEventListener("keyup", function (event) {
-    event.preventDefault();     //evita que se haga submit cada vez que se ejecuta la funcion
-    tablaOpciones.innerHTML = "";
-    if (barraBusqueda.value == 0) {
-        tablaOpciones.style.visibility = "hidden";
+  event.preventDefault(); //evita que se haga submit cada vez que se ejecuta la funcion
+  tablaOpciones.innerHTML = "";
+  if (barraBusqueda.value == 0) {
+    tablaOpciones.style.visibility = "hidden";
+  } else {
+    tablaOpciones.style.visibility = "";
+    const palabraClave = barraBusqueda.value.toLowerCase(); //recoje lo que hay escrito en la barra de busqueda
+    const menuItems = document.querySelectorAll(".menu-item"); //recoje todos los items con la clase "menu-item"
+    const results = [];
+
+    //por cada elemento de menuItems...
+    menuItems.forEach(function (menuItem) {
+      const menuItemText = menuItem.textContent.toLowerCase();
+      if (menuItemText.indexOf(palabraClave) !== -1) {
+        //comprueba si la palabra clave es un elemento del menu, si no lo es la funcion devuelve -1
+        divResult = menuItem.parentNode;
+        results.push(divResult);
+      }
+    });
+    if (results.length == 0) {
+      tablaOpciones.innerHTML = "No coincide ningun elemento";
     } else {
-        tablaOpciones.style.visibility = "";
-        const palabraClave = barraBusqueda.value.toLowerCase(); //recoje lo que hay escrito en la barra de busqueda
-        const menuItems = document.querySelectorAll(".menu-item");  //recoje todos los items con la clase "menu-item"
-        const results = [];
-
-        //por cada elemento de menuItems...
-        menuItems.forEach(function (menuItem) {
-            const menuItemText = menuItem.textContent.toLowerCase();
-            if (menuItemText.indexOf(palabraClave) !== -1) { //comprueba si la palabra clave es un elemento del menu, si no lo es la funcion devuelve -1
-                divResult = menuItem.parentNode;
-                results.push(divResult);
-            }
-        });
-        if (results.length == 0) {
-            tablaOpciones.innerHTML = "No coincide ningun elemento"
-        } else {
-            displayResults(results);
-        }
+      displayResults(results);
     }
-
-
+  }
 });
 
 function displayResults(results) {
-    results.forEach(function (result) {
-        const clon = result.cloneNode(true);
-        clon.classList.add("searchResult")
-        clon.insertAdjacentHTML('beforeend', '<button class="btnAddCesta" onclick="">Añadir al carrito</button>')
-        tablaOpciones.appendChild(clon);
-    });
+  results.forEach(function (result) {
+    const clon = result.cloneNode(true);
+    clon.classList.add("searchResult");
+    clon.insertAdjacentHTML(
+      "beforeend",
+      '<img src="../resources/cart-plus-solid (1).svg" alt="" class="btnAddCesta" onclick=""/>'
+    );
+    tablaOpciones.appendChild(clon);
+  });
 }
