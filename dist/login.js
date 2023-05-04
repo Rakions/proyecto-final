da3ecd392b5d74604d0b6bccacef6394840ad00a
Rakions <ogmohammed2004@hotmail.com>
2023-05-05 01:07:34 +0200

    Login y register terminado

--- dist/login.js
+++ dist/login.js
@@ -1,2 +1,10 @@
 const regexMain = "/^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/";
 
+const login = document.querySelector(".login-sign-in");
+const register = document.querySelector(".login-register");
+
+function switchRegister(){
+  login.classList.toggle("hidden")
+  register.classList.toggle("hidden")
+}
+
