94759359b908c11b68490ad48904a87db7893f77
Rakions <ogmohammed2004@hotmail.com>
2023-05-10 04:35:44 +0200

    insetado usuario empleado de prueba

--- sql/insertar_usuarios.sql
+++ sql/insertar_usuarios.sql
@@ -1,3 +1,7 @@
 INSERT into Cafe_users values (1,'Omar','Gomez','ogmohammed004@gmail.com','123456','Rakions',636723224,to_date('05/05/2023' , 'dd/mm/yyyy'));
 
 select * from cafe_users;
+
+insert into employees values (1,'omar','gomez','ausjdbaujbsd','asydgvavusd','12836192863', 19723,'manager',to_date('05/05/2023' , 'dd/mm/yyyy'));
+
+select * from employees;
