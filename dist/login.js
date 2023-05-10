31dc29e7e115827d7e26d74de00b3396489deb47
Zergen03 <75618128+Zergen03@users.noreply.github.com>
2023-05-10 20:56:15 +0200

    YA FUNCIONA LA CONEXION CON JAVA

--- dist/login.js
+++ dist/login.js
@@ -8,3 +8,24 @@ function switchRegister(){
   register.classList.toggle("hidden")
 }
 
+
+
+
+
+function pruebaConexion(){
+  
+  const xhr = new XMLHttpRequest();
+xhr.open("GET", "http://localhost:4000/API/cafeusuarios/consultar");
+xhr.send();
+xhr.responseType = "json";
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    const data = xhr.response;
+    console.log(data);
+    var imprimir = JSON.stringify(data)
+    alert(imprimir)
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
+}
