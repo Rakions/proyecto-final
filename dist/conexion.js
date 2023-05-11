172eb16b51428ea9bc2d94f3b720f91740c6eecf
Zergen03 <75618128+Zergen03@users.noreply.github.com>
2023-05-11 14:53:18 +0200

    añadida conexion con servidor

---
+++ dist/conexion.js
@@ -0,0 +1,18 @@
+function conexion(ruta, datos) {
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:4000/API/" + ruta + "?" + datos);
+    xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            const data = xhr.response;
+            console.log(data)
+            return data;
+        } else {
+            console.log(`Error: ${xhr.status}`);
+        }
+    };
+}
+
+
