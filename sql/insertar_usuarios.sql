3b3657fd5a967352d5239280af1d23fbea194fe9
Zergen03 <75618128+Zergen03@users.noreply.github.com>
2023-05-11 16:56:22 +0200

    añadido otro usuario a insertar usuarios

--- sql/insertar_usuarios.sql
+++ sql/insertar_usuarios.sql
@@ -1,7 +1,8 @@
 INSERT into Cafe_users values (1,'Omar','Gomez','ogmohammed004@gmail.com','123456','Rakions',636723224,to_date('05/05/2023' , 'dd/mm/yyyy'));
+INSERT into Cafe_users values (2,'Alex','Izquierdo','a26927@svalero.com','654321','Zergen',123456789,to_date('11/05/2023' , 'dd/mm/yyyy'));
 
 select * from cafe_users;
 
-insert into employees values (2,'omar','gomez','ausjdbaujbsd','asydgvavusd','12836192863', 19723,'manager',to_date('05/05/2023' , 'dd/mm/yyyy'));
+insert into employees values (1,'omar','gomez','ausjdbaujbsd','asydgvavusd','12836192863', 19723,'manager',to_date('05/05/2023' , 'dd/mm/yyyy'));
 
 select * from employees;
