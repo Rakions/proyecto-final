cd836483f1541296c6895650294affebb5241dd9
Rakions <ogmohammed2004@hotmail.com>
2023-05-11 22:34:14 +0200

    metodo login provisional

--- dist/login.js
+++ dist/login.js
@@ -8,3 +8,9 @@ function switchRegister(){
   register.classList.toggle("hidden")
 }
 
+async function logJSONData(){
+  const response = await fetch("http://localhost:4000/API/cafeusuarios/consultar",{method: 'GET'},{mode: 'no-cors'});
+  const jsonData = await response.json();
+  console.log(jsonData[0]['user_name']);
+}
+
