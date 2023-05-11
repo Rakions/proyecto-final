7a558ea984415e3b4c7c62d547bbc17f5f95b71e
Zergen03 <75618128+Zergen03@users.noreply.github.com>
2023-05-11 14:52:58 +0200

    añadida conexion con servidor

--- dist/login.js
+++ dist/login.js
@@ -8,24 +8,14 @@ function switchRegister(){
   register.classList.toggle("hidden")
 }
 
-
-
-
-
-function pruebaConexion(){
-  
-  const xhr = new XMLHttpRequest();
-xhr.open("GET", "http://localhost:4000/API/cafeusuarios/consultar");
-xhr.send();
-xhr.responseType = "json";
-xhr.onload = () => {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    const data = xhr.response;
-    console.log(data);
-    var imprimir = JSON.stringify(data)
-    alert(imprimir)
-  } else {
-    console.log(`Error: ${xhr.status}`);
+function comprobarLoging(email, password){
+  var query = conexion("usuarios/buscar", ("id="+email));
+  alert(query)
+  if(query != null){
+    console.log("existe")
+  }else{
+    console.log("no existe")  
   }
-};
 }
+
+
