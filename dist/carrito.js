4615201d39a9feddecf43b583e20f0edb6368daf
Zergen03 <75618128+Zergen03@users.noreply.github.com>
2023-05-15 21:33:48 +0200

    preparando carrito

---
+++ dist/carrito.js
@@ -0,0 +1,28 @@
+function agregarCarrito(){
+    
+}
+
+
+
+
+
+
+
+<li class="grid grid-cols-3 w-full px-4 cart_item list1">
+<div class="flex items-center">
+  <img src="../resources/cafe-con-leche.jpg" alt="">
+  <p class="max-w-[100px] ml-4 product_name">Lorem, ipsum dolor.</p>
+</div>
+<div class="flex items-center justify-evenly">
+  <h1 class="item_price1">2,50€</h1>
+  <div>
+    <button class="w-4">+</button>
+    <span class="product_amount1">1</span>
+    <button class="w-4">-</button>
+  </div>
+</div>
+<div class="flex items-center justify-evenly">
+  <div id="precio-total" class="text-center">0.00€</div>
+  <button>X</button>
+</div>
+</li>
