2c923a9a364a5ee2ae0002f573a1e85b02fdf3bc
Zergen03 <75618128+Zergen03@users.noreply.github.com>
2023-05-16 22:48:47 +0200

    completado carrito

--- dist/carrito.js
+++ dist/carrito.js
@@ -1,59 +1,90 @@
-async function agregarCarrito(product_id) {
-  var result = await conexion("products/buscar", ("id="+product_id))
-  const li = document.createElement("li");
-  const divProduct = document.createElement("div");
-  const divPrice = document.createElement("div");
-  const divTotal = document.createElement("div");
+function agregarCarrito(product_id) {
+  var productos = new Array();
+  if (localStorage.getItem('carrito') != null) {
+    productos = JSON.parse(localStorage.getItem('carrito'));
+    productos = productos;
+    productos.push(product_id);
+  } else {
+    productos[0] = product_id
+  }
+  localStorage.setItem('carrito', JSON.stringify(productos));
+}
 
-  divProduct.classList.add("flex items-center");
-  divPrice.classList.add("flex items-center justify-evenly");
-  divTotal.classList.add("flex items-center justify-evenly");
-  console.log(result)
-  // divProduct.insertAdjacentElement(
-  //   "afterbegin",
-  //   "img src=" + result[""]
-  // )
-  
+function eliminarCarrito(product_id, todo) {
+  var productos = new Array();
+  productos = JSON.parse(localStorage.getItem('carrito'));
+  if (!todo) {
+    let eliminado = false;
+    productos.splice(productos.indexOf(product_id), 1);
+  }else{
+    productos = productos.filter(product => product != product_id);
+  }
+  if(productos.indexOf(product_id) == -1){
+    document.getElementById("product" + product_id).style.display = "none"
+  }
+  localStorage.setItem('carrito', JSON.stringify(productos));
+}
+
+
+async function construirCarrito() {
+
+  var productos = localStorage.getItem('carrito');
+  productos = JSON.parse(productos);
+
+  if (productos != null) {
+    productos.forEach(async function (product_id) {
+
+      var result = await conexion("products/buscar", ("id=" + product_id));
+      const carrito = document.getElementById("carrito-container");
+      const divId = "product" + product_id;
+
+      if (document.getElementById(divId) == null) {
+        carrito.innerHTML +=
+          `
+          <li class="grid grid-cols-3 w-full px-4 cart_item list1" id="${divId}">
+            <div class="flex items-center">
+              <img src=" ${result[0]["image_url"]}">
+              <p class="max-w-[100px] ml-4 product_name">${result[0]["product_name"]}</p>
+            </div>
+            <div class="flex items-center justify-evenly">
+              <h1 class="item_price1">${result[0]["price"]}€</h1>
+              <div>
+                <button class="w-4 text-xl" onclick="modificarCantidad(${product_id}, 1)">+</button>
+                <span class="product_amount1" id="cantidad${product_id}">1</span>
+                <button class="w-4 text-xl" onclick="modificarCantidad(${product_id}, (-1))">-</button>
+              </div>
+            </div>
+            <div class="flex items-center justify-evenly">
+              <div id="precio-total${product_id}" class="text-center">${result[0]["price"]}€</div>
+              <button onclick="modificarCantidad(${product_id}, 0)">X</button>
+            </div>
+          </li>
+          `
+
+        document.getElementById("precio-total" + product_id).innerHTML = result[0]["price"] * document.getElementById("cantidad" + product_id).innerHTML
+      } else {
+        document.getElementById("cantidad" + product_id).innerHTML = parseInt(document.getElementById("cantidad" + product_id).innerHTML) + 1;
+        document.getElementById("precio-total" + product_id).innerHTML = result[0]["price"] * document.getElementById("cantidad" + product_id).innerHTML
+      }
+      document.getElementById("subtotal").innerHTML = parseInt(document.getElementById("subtotal").innerHTML) + result[0]["price"] + "€";
+      document.getElementById("Total").innerHTML = document.getElementById("subtotal").innerHTML
+    });
+  }
+}
+
+
+function modificarCantidad(product, action) {
+  var cantidad = document.getElementById("cantidad" + product);
+  if(action > 0){
+    agregarCarrito(product);
+  }else if(action < 0){
+    eliminarCarrito(product, false)
+  }else{
+    eliminarCarrito(product, true)
+  }
+  cantidad.innerHTML = parseInt(cantidad.innerHTML) + action;
 }
 
 
 
-// function displayResults(results) {
-//   results.forEach(function (result) {
 
-//     var div = document.createElement("div");
-//     div.innerHTML = result["product_name"];
-//     div.innerHTML += "<p>" + result["price"] + "€</p>"
-//     div.insertAdjacentHTML(
-//       "afterbegin",
-//       '<img src="' + result["image_url"] + '"/>'
-//     );
-//     div.classList.add("searchResult");
-//     div.insertAdjacentHTML(
-//       "beforeend",
-//       '<img src="../resources/cart-plus-solid.svg" alt="" class="btnAddCesta" onclick="agregarCarrito(result[\"product_id\"])"/>'
-//     );
-//     tablaOpciones.appendChild(div)
-//   });
-// }
-
-
-
-{/* <li class="grid grid-cols-3 w-full px-4 cart_item list1">
-  <div class="flex items-center">
-    <img src="../resources/cafe-con-leche.jpg">
-      <p class="max-w-[100px] ml-4 product_name">Lorem, ipsum dolor.</p>
-  </div>
-  <div class="flex items-center justify-evenly">
-    <h1 class="item_price1">2,50€</h1>
-    <div>
-      <button class="w-4">+</button>
-      <span class="product_amount1">1</span>
-      <button class="w-4">-</button>
-    </div>
-  </div>
-  <div class="flex items-center justify-evenly">
-    <div id="precio-total" class="text-center">0.00€</div>
-    <button>X</button>
-  </div>
-</li> */}
