ce7a396a6d4708581e5fd9c8c66ffef6d46c0475
Rakions <ogmohammed2004@hotmail.com>
2023-05-23 20:29:59 +0200

    Estilos generales creados, funcionalidades pendientes

--- dist/navbar.js
+++ dist/navbar.js
@@ -2,3 +2,8 @@ function navbarMobile() {
   const navbarMovil = document.querySelector(".header-movil");
   navbarMovil.classList.toggle("active");
 }
+
+function navbarDashboard(){
+  const navbarDashboard = document.querySelector(".navegacion_dashboard");
+  navbarDashboard.classList.toggle("active");
+}
