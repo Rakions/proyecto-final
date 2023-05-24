418874a7aee65538d1ad7ab615b2f94f88085ba3
Zergen03 <75618128+Zergen03@users.noreply.github.com>
2023-05-25 01:16:50 +0200

    completado carrito con la base de datos (creo)

--- dist/carrito.js
+++ dist/carrito.js
@@ -21,7 +21,7 @@ async function agregarCarrito(product_id) {
         "product_id": product_id,
         "quantity": (orderDetails[0]["quantity"]) + 1
       }
-      conexionPut("orders_details/modificar/quantity", dataDetails)
+      await conexionPut("orders_details/modificar/quantity", dataDetails)
     } else {
       var dataDetails = {
         "orders_id": order["orders_id"],
@@ -63,84 +63,101 @@ async function construirCarrito() {
     productos.forEach(async function (product) {
 
 
-      var result = await conexion("products/buscar", ("id=" + product["product_id"]));
+      var productInfo = await conexion("products/buscar", ("id=" + product["product_id"]));
 
       const carrito = document.getElementById("carrito-container");
 
       carrito.innerHTML +=
         `
-          <li class="grid grid-cols-3 w-full px-4 cart_item list1" id= product"${product["product_id"]}">
+          <li class="grid grid-cols-3 w-full px-4 cart_item list1" id= product${product["product_id"]}>
             <div class="flex items-center">
-              <img src=" ${result[0]["image_url"]}">
-              <p class="max-w-[100px] ml-4 product_name">${result[0]["product_name"]}</p>
+              <img src=" ${productInfo[0]["image_url"]}">
+              <p class="max-w-[100px] ml-4 product_name">${productInfo[0]["product_name"]}</p>
             </div>
             <div class="flex items-center justify-evenly">
-              <h1 class="item_price1">${result[0]["price"]}€</h1>
+              <h1 class="item_price">${productInfo[0]["price"]}€</h1>
               <div>
-                <button class="w-4 text-xl" onclick="modificarCantidad(${product["product_id"]}, 1)">+</button>
+                <button class="w-4 text-xl" onclick="modificarCantidad(${product["orders_id"]}, ${product["product_id"]}, 1)">+</button>
                 <span class="product_amount1" id="cantidad${product["product_id"]}">${product["quantity"]}</span>
-                <button class="w-4 text-xl" onclick="modificarCantidad(${product["product_id"]}, (-1))">-</button>
+                <button class="w-4 text-xl" onclick="modificarCantidad(${product["orders_id"]}, ${product["product_id"]}, (-1))">-</button>
               </div>
             </div>
             <div class="flex items-center justify-evenly">
-              <div id="precio-total${product}" class="text-center">${result[0]["price"]}€</div>
-              <button onclick="modificarCantidad(${product}, 0)">X</button>
+              <div id="precio-total${product["product_id"]}" class="text-center">${productInfo[0]["price"] * product["quantity"]}€</div>
+              <button onclick="modificarCantidad(${product["orders_id"]}, ${product["product_id"]}, 0)">X</button>
             </div>
           </li>
           `
 
-      // if (document.getElementById("product" + product["product_id"]) == null) {
-
-
-      //   document.getElementById("precio-total").innerHTML = result[0]["price"] * document.getElementById("cantidad" + product["product_id"]).innerHTML
-      // } else {
-      //   document.getElementById("cantidad" + product["product_id"]).innerHTML = parseInt(document.getElementById("cantidad" + product_id).innerHTML) + 1;
-      //   document.getElementById("precio-total" + product["product_id"]).innerHTML = result[0]["price"] * document.getElementById("cantidad" + product["product_id"]).innerHTML
-      // }
-      precioTotal(result[0]["price"] * product["quantity"]);
+      precioTotal(productInfo[0]["price"] * product["quantity"]);
     });
   }
 
 
 
 }
-function precioTotal(precio, action) {
+function precioTotal(precio) {
   document.getElementById("subtotal").innerHTML = parseInt(document.getElementById("subtotal").innerHTML) + precio + "€";
   document.getElementById("Total").innerHTML = document.getElementById("subtotal").innerHTML
 }
 
 
-async function modificarCantidad(product, action) {
-  
-  var result = await conexion("products/buscar", "id=" + product)
+async function modificarCantidad(order, product, action) {
+
+  product = (await conexion("orders_details/buscarOrderProduct", "orders_id=" + order + "&product_id=" + product))[0];
+
   action = (action == undefined ? 1 : action);
-  var precio = result[0]["price"] * action;
-  var cantidad = document.getElementById("cantidad" + product);
-  var total = document.getElementById("precio-total" + product);
+
   if (action > 0) {
-    agregarCarrito(product);
+    await agregarCarrito(product["product_id"]);
   } else if (action < 0) {
-    eliminarCarrito(product, false);
+    await eliminarCarrito(product, false);
   } else {
-    eliminarCarrito(product, true);
+    await eliminarCarrito(product, true);
   }
-  cantidad.innerHTML = parseInt(cantidad.innerHTML) + precio;
-  total.innerHTML = parseInt(total.innerHTML) + precio;
-  precioTotal(precio, action)
+
+  product = (await conexion("orders_details/buscarOrderProduct", "orders_id=" + order + "&product_id=" + product["product_id"]))[0];
+
+  var productInfo = await conexion("products/buscar", "id=" + product["product_id"])
+
+  var cantidad = product["quantity"]
+  var total = productInfo[0]["price"] * cantidad;
+
+  document.getElementById("cantidad" + [product["product_id"]]).innerHTML = cantidad
+  document.getElementById("precio-total" + [product["product_id"]]).innerHTML = total + "€"
+  precioTotal(productInfo[0]["price"] * action)
 }
 
-function eliminarCarrito(product_id, todo) {
-  var productos = new Array();
-  productos = JSON.parse(localStorage.getItem('carrito'));
-  if (!todo) {
-    productos.splice(productos.indexOf(product_id), 1);
+async function eliminarCarrito(product, todo) {
+
+
+  if (todo) {
+    var data = {
+      "orders_id": product["orders_id"],
+      "product_id": product["product_id"]
+    }
+    await conexionDelete("orders_details/eliminar", data);
+    document.getElementById("product" + product["product_id"]).style.display = "none"
   } else {
-    productos = productos.filter(product => product != product_id);
+    var dataDetails = {
+      "orders_id": product["orders_id"],
+      "product_id": product["product_id"],
+      "quantity": (product["quantity"]) - 1
+    }
+    conexionPut("orders_details/modificar/quantity", dataDetails);
+    var productInfo = await conexion("products/buscar", "id=" + product["product_id"])
+
+    document.getElementById("cantidad" + [product["product_id"]]).innerHTML = product["quantity"] - 1
+    document.getElementById("precio-total" + [product["product_id"]]).innerHTML = productInfo[0]["price"] * product["quantity"];
+    if (product["quantity"] - 1 <= 0) {
+      var data = {
+        "orders_id": product["orders_id"],
+        "product_id": product["product_id"]
+      }
+      await conexionDelete("orders_details/eliminar", data);
+      document.getElementById("product" + product["product_id"]).style.display = "none"
+    }
   }
-  if (productos.indexOf(product_id) == -1) {
-    document.getElementById("product" + product_id).style.display = "none"
-  }
-  localStorage.setItem('carrito', JSON.stringify(productos));
 }
 
 //-------------------------CHECKOUT----------------------//
