4afac7fc588bcb05178243da0245e6a1d334e772
Rakions <ogmohammed2004@hotmail.com>
2023-05-26 22:03:59 +0200

    agregados insert de cafes

---
+++ sql/creacion_categorias.sql
@@ -0,0 +1,4 @@
+insert into categories (category_name) values ('Cafe');
+insert into categories (category_name) values ('Bebidas frias');
+insert into categories (category_name) values ('Tes');
+insert into categories (category_name) values ('Bocadillos');
